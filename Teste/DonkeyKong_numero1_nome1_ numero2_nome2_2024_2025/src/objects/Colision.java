1d179598697d3ba0f94152a7a5bc6ad3aff33300
TiagoNpg <tiago.nunes2626@gmail.com>
2024-12-04 20:24:04 +0000

    remove unnecessary functions

--- Teste/DonkeyKong_numero1_nome1_ numero2_nome2_2024_2025/src/objects/Colision.java
+++
@@ -1,8 +0,0 @@
-package objects;
-
-public interface Colision { //Definir em cada objeto oq acontece com a interação do objeto
-
-    void interactisWithHero(); //interage com o herói?
-    void interaction(); // CARNE, SWORD, TRAP...
-
-}
